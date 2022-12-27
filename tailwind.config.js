5458ee9e3fb0460e45176263098ab4962c0e6f66
Michael Young <michaeljy@gmail.com>
2022-12-27 14:30:18 -0600

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#fefefe',
+        black: '#1d1d1d',
+      },
+      container: { padding: 20, center: true },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ addUtilities, addComponents }) => {
+      addUtilities({
+        '.center': {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      });
+      addComponents({
+        '.card': {
+          '@apply bg-zinc-800 rounded-lg border border-zinc-700 p-4': {},
+        },
+        '.text-link': {
+          '@apply text-sky-500 hover:underline': {},
+        },
+      });
+    }),
+  ],
+};
