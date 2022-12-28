5c840018f36bf80048e681618fdd58cc31850862
Michael Young <michaeljy@gmail.com>
2022-12-28 13:09:53 -0600

    Remove unnecessary comments

--- pages/api/job-requests/index.ts
+++ pages/api/job-requests/index.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Prisma } from '@prisma/client';
 import { isEmpty } from 'lodash-es';
 import type { NextApiRequest, NextApiResponse } from 'next';
